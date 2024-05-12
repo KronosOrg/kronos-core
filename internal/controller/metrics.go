1d986b31d8fb49fa4d05a76cad40fd450983263d
Ismail Abdelkefi <ismail.abdelkefi@wecraft.tn>
2024-05-12 13:50:37 +0100

    created new metric for in-depth information

--- internal/controller/metrics.go
+++ internal/controller/metrics.go
@@ -7,12 +7,17 @@ import (
 
 type Metrics struct {
 	ScheduleInfo *prometheus.GaugeVec
+	InDepthScheduleInfo *prometheus.GaugeVec
 }
 
 func RegisterMetrics() Metrics {
 	sleepInfoMetrics := Metrics{
 		ScheduleInfo: prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Name: "schedule_info",
+			Help: "Current schedule information",
+		}, []string{"name", "namespace"}),
+		InDepthScheduleInfo: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "indepth_schedule_info",
 			Help: "Current schedule information",
 		}, []string{"name", "namespace", "status", "reason", "handled_resources", "next_operation"}),
 	}
@@ -22,6 +27,7 @@ func RegisterMetrics() Metrics {
 func (additionalMetrics Metrics) MustRegister(registry metrics.RegistererGatherer) Metrics {
 	registry.MustRegister(
 		additionalMetrics.ScheduleInfo,
+		additionalMetrics.InDepthScheduleInfo,
 	)
 	return additionalMetrics
 }
